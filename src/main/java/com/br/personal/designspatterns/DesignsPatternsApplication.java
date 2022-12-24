6db7e645033d0afb0bfc97a677bf1269422a033c
roger.guillermo <roger.guillermo@ambevtech.com.br>
2022-12-24 10:45:09 -0300

    Initial commit

---
+++ src/main/java/com/br/personal/designspatterns/DesignsPatternsApplication.java
@@ -0,0 +1,13 @@
+package com.br.personal.designspatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesignsPatternsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesignsPatternsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/personal/designspatterns/DesignsPatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.personal.designspatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignsPatternsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
